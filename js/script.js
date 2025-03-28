67d9a1fb66de521f3cca424c73a2d345e925cf95
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-28 15:11:33 +0100

    [UPD] object array created

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 //esercizio 1
 //andiamo a creare un array di oggetti
+const Bike = [
+    {
+        nome: "Orbea Orca M30i",
+        peso: 7.0
+    },
+    {
+        nome: "Merida Reacto 4000",
+        peso: 5.6
+    },
+    {
+        nome: "AERO C:68X SLX",
+        peso: 4.9
+    },
+    {
+        nome: "Allez E5 Sport",
+        peso: 6.4
+    },
+    {
+        nome: "Scott Addict RC 20",
+        peso: 4.5
+    },
+    {
+        nome: "Cube Litening AERO C:68X",
+        peso: 3.9
+    },
+]
+
+console.log(bike)
